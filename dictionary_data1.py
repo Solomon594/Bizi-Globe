a584a9abaf1c64c008b683c7f7fdac43efeda66f
Solomon594 <99629676+Solomon594@users.noreply.github.com>
2023-11-02 07:54:28 +0000

    Dictionary for medical dict

---
+++ dictionary_data1.py
@@ -0,0 +1,53 @@
+dictionary_data = {
+    "allergy": {
+        "Definition": "An allergy is an abnormal immune response to a typically harmless substance, such as pollen, pet dander, or certain foods. Common symptoms include sneezing, itching, and skin rashes.",
+        "Medications": ["Antihistamines", "Epinephrine"],
+        "Diagnostic Tests": ["Skin Prick Test", "Blood Test"]
+    },
+    "bladder cancer": {
+        "Definition": "Bladder cancer is a type of cancer that occurs in the lining of the bladder. It often causes blood in the urine and frequent urination.",
+        "Medications": ["BCG vaccine", "Chemotherapy"],
+        "Diagnostic Tests": ["Cystoscopy", "Urinalysis"]
+    },
+    "macular degeneration": {
+        "Definition": "Macular degeneration is an eye condition that causes damage to the macula, leading to vision loss in the center of the visual field.",
+        "Medications": ["Anti-VEGF drugs", "Photodynamic therapy"],
+        "Diagnostic Tests": ["Optical coherence tomography", "Fluorescein angiography"]
+    },
+    "pneumonia": {
+        "Definition": "Pneumonia is an infection that affects one or both lungs. It causes the air sacs of the lungs to fill up with fluid or pus, leading to symptoms like cough, fever, and difficulty breathing.",
+        "Medications": ["Antibiotics", "Antiviral drugs"],
+        "Diagnostic Tests": ["Chest X-ray", "Sputum culture"]
+    },
+    "ectopic pregnancy": {
+        "Definition": "An ectopic pregnancy is a pregnancy that occurs outside the uterus, typically in the fallopian tube. It can be a life-threatening condition.",
+        "Medications": ["Methotrexate", "Surgery"],
+        "Diagnostic Tests": ["Transvaginal ultrasound", "Blood hCG levels"]
+    },
+    "kidney stones": {
+        "Definition": "Kidney stones are hard deposits of minerals and salts that form in the kidneys. They can cause severe pain when they pass through the urinary tract.",
+        "Medications": ["Pain relievers", "Alpha blockers"],
+        "Diagnostic Tests": ["CT scan", "Urinalysis"]
+    },
+    "hemolytic anemia": {
+        "Definition": "Hemolytic anemia is a condition in which the body destroys red blood cells faster than it can produce them, leading to anemia and related symptoms.",
+        "Medications": ["Corticosteroids", "Immunosuppressants"],
+        "Diagnostic Tests": ["Blood tests", "Coombs test"]
+    },
+    "syphilis": {
+        "Definition": "Syphilis is a chronic contagious disease caused by the bacterium Treponema pallidum. It can cause chancres, rashes, and systemic lesions in multiple stages if left untreated.",
+        "Medications": ["Penicillin", "Doxycycline"],
+        "Diagnostic Tests": ["VDRL test", "Darkfield microscopy"]
+    },
+    "diabetic kidney disease": {
+        "Definition": "Diabetic kidney disease, also known as diabetic nephropathy, is a complication of diabetes that affects the kidneys. It can lead to kidney damage and impaired function.",
+        "Medications": ["ACE inhibitors", "ARBs"],
+        "Diagnostic Tests": ["Urine albumin-to-creatinine ratio", "GFR estimation"]
+    },
+    "acute bronchitis": {
+        "Definition": "Acute bronchitis is an inflammation of the bronchial tubes, often caused by viral infections. It leads to symptoms like cough, chest discomfort, and mucus production.",
+        "Medications": ["Cough suppressants", "Bronchodilators"],
+        "Diagnostic Tests": ["Clinical evaluation", "Chest X-ray"]
+    }
+
+}
